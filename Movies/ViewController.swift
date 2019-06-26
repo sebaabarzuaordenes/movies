815d95d228f1aad12c121e9e8390ece75e804b20
Sebastian Abarzua <sabarzua@outlook.com>
2019-06-26 16:28:21 -0400

    [ADD] se agrega el proyecto al repositorio

---
+++ Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Sebastian Abarzua on 6/26/19.
+//  Copyright © 2019 squirrelweb.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
